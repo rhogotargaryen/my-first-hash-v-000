ac18cd420b6f4ae176e2c39fbf277582318f2b7b
Derek Haller <dhaller.personal@gmail.com>
2018-01-26 05:32:28 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,9 +18,7 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  return shipping_manifest["oil paintings"]
 end
 
 def adding
